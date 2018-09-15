0115d33e6cc72ebfe8647feb9bdb756692e281e6
Addison Luh <hueich@gmail.com>
2018-09-15 13:52:23 -0700

    Make gzip encoding handling optional.

--- server.go
+++ server.go
@@ -13,6 +13,7 @@ var (
 	hosts  = flag.String("hosts", "", "One or more comma-separated hosts to serve for")
 	port   = flag.Int("port", 8080, "Server port")
 	wwwDir = flag.String("wwwdir", "www", "Directory to serve")
+	gzip   = flag.Bool("gzip", false, "Handle gzip encoding")
 
 	regexAllowedIP = regexp.MustCompile(`^(127\.0\.0\.1|(192\.168|10\.\d+)\.\d+.\d+)$`)
 )
@@ -69,5 +70,9 @@ func main() {
 	hs := strings.Split(*hosts, ",")
 	log.Printf("Starting to listen on port=%d serving hosts=%s using directory=%s", *port, strings.Join(hs, ","), *wwwDir)
 	// TODO: Use TLS.
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), &filterHostHandler{hs, &gzipHandler{http.FileServer(http.Dir(*wwwDir))}}))
+	handler := http.FileServer(http.Dir(*wwwDir))
+	if *gzip {
+		handler = &gzipHandler{handler}
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), &filterHostHandler{hs, handler}))
 }
